c3681f428bc7461d333dc16b84966ef40ca29fbc
Florian Wetzels <florian@bau48-inf-ag-schweitzer-ipv4-0052.informatik.uni-kl.de>
2021-11-26 10:58:01 +0100

    removed new state file again, modified python statefile such that it works with ExtractSelection and now show full pipeline with matching

--- python/contourTreeAlignment.py
+++ python/contourTreeAlignment.py
@@ -13,12 +13,12 @@ ttkCinemaQuery.SQLStatement = """SELECT * FROM InputTable0 WHERE time=4.2"""
 ttkCinemaProductReader = TTKCinemaProductReader(Input=ttkCinemaQuery)
 
 # create a new 'TTK TopologicalSimplificationByPersistence'
-ttkTopologicalSimplificationByPersistence = TTKTopologicalSimplificationByPersistence(registrationName='TTKTopologicalSimplificationByPersistence', Input=ttkCinemaProductReader)
+ttkTopologicalSimplificationByPersistence = TTKTopologicalSimplificationByPersistence(Input=ttkCinemaProductReader)
 ttkTopologicalSimplificationByPersistence.InputArray = ['POINTS', 'nrrd']
 ttkTopologicalSimplificationByPersistence.PersistenceThreshold = 0.05
 
 # create a new 'TTK Merge and Contour Tree (FTM)'
-ttkMergeandContourTreeFTM = TTKMergeandContourTreeFTM(registrationName='TTKMergeandContourTreeFTM', Input=ttkTopologicalSimplificationByPersistence)
+ttkMergeandContourTreeFTM = TTKMergeandContourTreeFTM(Input=ttkTopologicalSimplificationByPersistence)
 ttkMergeandContourTreeFTM.ScalarField = ['POINTS', 'nrrd']
 
 # create a new 'TTK ContourTreeAlignment'
@@ -42,5 +42,48 @@ alignmentEdges = Calculator(Input=alignmentLayout)
 alignmentEdges.CoordinateResults = 1
 alignmentEdges.Function = 'iHat*Layout_Y+jHat*Scalar*3'
 
+# create a query selection
+QuerySelect(QueryString='(id == 16)', Source=alignmentEdges, FieldType='POINT', InsideOut=0)
+
+# create a new 'Extract Selection'
+selectedVertex = ExtractSelection(Input=alignmentEdges)
+
+# create a new 'TTK GridLayout'
+segmentationsGrid = TTKGridLayout(Input=OutputPort(ttkMergeandContourTreeFTM,2))
+segmentationsGrid.ColumnGap = 10.0
+segmentationsGrid.RowGap = 10.0
+
+# create a new 'TTK ForEach'
+ttkForEach = TTKForEach(Input=segmentationsGrid)
+ttkForEach.InputArray = ['POINTS', 'SegmentationId']
+ttkForEach.ImageExtent = [0, 127, 0, 255, 0, 0]
+
+# create a new 'Merge Blocks'
+mergeBlocks = MergeBlocks(Input=ttkForEach)
+
+# create a new 'TTK ArrayEditor'
+passSegmentationIDs = TTKArrayEditor(Target=mergeBlocks,
+    Source=selectedVertex)
+passSegmentationIDs.EditorMode = 'Add Arrays from Source'
+passSegmentationIDs.TargetAttributeType = 'Field Data'
+passSegmentationIDs.SourcePointDataArrays = ['segmentationIDs']
+passSegmentationIDs.TargetArray = ['POINTS', 'SegmentationId']
+
+# create a new 'TTK Extract'
+extractMatchedGeometry = TTKExtract(Input=passSegmentationIDs)
+extractMatchedGeometry.ExtractionMode = 'Geometry'
+extractMatchedGeometry.Expression = '{segmentationIDs[{_ttk_IterationInfo[0]}]}'
+extractMatchedGeometry.ImageExtent = [2147483647, -2147483647, 2147483647, -2147483647, 2147483647, -2147483647]
+extractMatchedGeometry.InputArray = ['POINTS', 'SegmentationId']
+
+# create a new 'TTK BlockAggregator'
+ttkBlockAggregator = TTKBlockAggregator(Input=extractMatchedGeometry)
+
+# create a new 'TTK EndFor'
+ttkEndFor = TTKEndFor(Data=ttkBlockAggregator,
+    For=ttkForEach)
+
 # save the output
 SaveData('ContourTreeAlignment.vtu', alignmentEdges)
+SaveData('Segmentations.vtm', segmentationsGrid)
+SaveData('MatchedRegions.vtm', ttkEndFor)
