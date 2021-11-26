43b4b6d613361601a1aa05e4dd81f3fa0b463841
Jonas Lukasczyk <jl@jluk.de>
2021-11-26 15:54:18 +0100

    Made python state file of ntg compatible with pv 5.10

--- states/nestedTrackingFromOverlap.py
+++ states/nestedTrackingFromOverlap.py
@@ -2,6 +2,7 @@
 
 #### import the simple module from the paraview
 from paraview.simple import *
+
 #### disable automatic camera reset on 'Show'
 paraview.simple._DisableFirstRenderCameraReset()
 
@@ -67,26 +68,26 @@ SetActiveView(renderView2)
 # ----------------------------------------------------------------
 
 # create a new 'TTK CinemaReader'
-viscousFingerscdb = TTKCinemaReader(registrationName='ViscousFingers.cdb', DatabasePath='ViscousFingers.cdb')
+ttkCinemaReader1 = TTKCinemaReader(DatabasePath='ViscousFingers.cdb')
 
 # create a new 'TTK CinemaQuery'
-ttkCinemaQuery1 = TTKCinemaQuery(registrationName='TTKCinemaQuery1', InputTable=viscousFingerscdb)
+ttkCinemaQuery1 = TTKCinemaQuery(InputTable=ttkCinemaReader1)
 ttkCinemaQuery1.SQLStatement = """SELECT * FROM InputTable0
 WHERE Sim='run01'
 ORDER BY Time
 LIMIT 100 OFFSET 20"""
 
 # create a new 'TTK ForEach'
-ttkForEach1 = TTKForEach(registrationName='TTKForEach1', Input=ttkCinemaQuery1)
+ttkForEach1 = TTKForEach(Input=ttkCinemaQuery1)
 
 # create a new 'TTK CinemaProductReader'
-ttkCinemaProductReader1 = TTKCinemaProductReader(registrationName='TTKCinemaProductReader1', Input=ttkForEach1)
+ttkCinemaProductReader1 = TTKCinemaProductReader(Input=ttkForEach1)
 
 # create a new 'Merge Blocks'
-mergeBlocks1 = MergeBlocks(registrationName='MergeBlocks1', Input=ttkCinemaProductReader1)
+mergeBlocks1 = MergeBlocks(Input=ttkCinemaProductReader1)
 
 # create a new 'Clip'
-clip1 = Clip(registrationName='Clip1', Input=mergeBlocks1)
+clip1 = Clip(Input=mergeBlocks1)
 clip1.ClipType = 'Plane'
 clip1.HyperTreeGridClipper = 'Plane'
 clip1.Value = 38.38161849975586
@@ -96,117 +97,112 @@ clip1.ClipType.Origin = [31.5, 31.5, 50.0]
 clip1.ClipType.Normal = [0.0, 0.0, 1.0]
 
 # create a new 'Threshold'
-threshold2 = Threshold(registrationName='Threshold2', Input=clip1)
-threshold2.Scalars = ['POINTS', 'NONE']
-threshold2.Scalars = ['POINTS', 'ImageFile']
-threshold2.ThresholdRange = [28.0, 2000.0]
-
-# create a new 'Connectivity'
-connectivity2 = Connectivity(registrationName='Connectivity2', Input=threshold2)
-
-# create a new 'TTK BlockAggregator'
-ttkBlockAggregator6 = TTKBlockAggregator(registrationName='TTKBlockAggregator6', Input=connectivity2)
-ttkBlockAggregator6.ForceReset = 1
-
-# create a new 'Threshold'
-threshold3 = Threshold(registrationName='Threshold3', Input=clip1)
-threshold3.Scalars = ['POINTS', 'NONE']
-threshold3.Scalars = ['POINTS', 'ImageFile']
-threshold3.ThresholdRange = [32.0, 2000.0]
-
-# create a new 'Connectivity'
-connectivity3 = Connectivity(registrationName='Connectivity3', Input=threshold3)
-
-# create a new 'TTK BlockAggregator'
-ttkBlockAggregator7 = TTKBlockAggregator(registrationName='TTKBlockAggregator7', Input=connectivity3)
-ttkBlockAggregator7.ForceReset = 1
-
-# create a new 'Threshold'
-threshold1 = Threshold(registrationName='Threshold1', Input=clip1)
-threshold1.Scalars = ['POINTS', 'NONE']
+threshold1 = Threshold(Input=clip1)
 threshold1.Scalars = ['POINTS', 'ImageFile']
-threshold1.ThresholdRange = [20.0, 2000.0]
+threshold1.ThresholdMethod = "Above Upper Threshold"
+threshold1.UpperThreshold = 20.0
+
+# create a new 'Threshold'
+threshold2 = Threshold(Input=clip1)
+threshold2.Scalars = ['POINTS', 'ImageFile']
+threshold2.ThresholdMethod = "Above Upper Threshold"
+threshold2.UpperThreshold = 28.0
+
+# create a new 'Threshold'
+threshold3 = Threshold(Input=clip1)
+threshold3.Scalars = ['POINTS', 'ImageFile']
+threshold3.ThresholdMethod = "Above Upper Threshold"
+threshold3.UpperThreshold = 32.0
 
 # create a new 'Connectivity'
-connectivity1 = Connectivity(registrationName='Connectivity1', Input=threshold1)
+connectivity1 = Connectivity(Input=threshold1)
+
+# create a new 'Connectivity'
+connectivity2 = Connectivity(Input=threshold2)
+
+# create a new 'Connectivity'
+connectivity3 = Connectivity(Input=threshold3)
 
 # create a new 'TTK BlockAggregator'
-ttkBlockAggregator5 = TTKBlockAggregator(registrationName='TTKBlockAggregator5', Input=connectivity1)
+ttkBlockAggregator5 = TTKBlockAggregator(Input=connectivity1)
 ttkBlockAggregator5.ForceReset = 1
 
 # create a new 'TTK BlockAggregator'
-ttkBlockAggregator1 = TTKBlockAggregator(registrationName='TTKBlockAggregator1', Input=[ttkBlockAggregator5, ttkBlockAggregator6, ttkBlockAggregator7])
+ttkBlockAggregator6 = TTKBlockAggregator(Input=connectivity2)
+ttkBlockAggregator6.ForceReset = 1
+
+# create a new 'TTK BlockAggregator'
+ttkBlockAggregator7 = TTKBlockAggregator(Input=connectivity3)
+ttkBlockAggregator7.ForceReset = 1
+
+# create a new 'TTK BlockAggregator'
+ttkBlockAggregator1 = TTKBlockAggregator(Input=[ttkBlockAggregator5, ttkBlockAggregator6, ttkBlockAggregator7])
 ttkBlockAggregator1.ForceReset = 1
 ttkBlockAggregator1.FlattenInput = 0
 
 # create a new 'TTK ArrayEditor'
-ttkArrayEditor1 = TTKArrayEditor(registrationName='TTKArrayEditor1', Target=ttkBlockAggregator1,
+ttkArrayEditor1 = TTKArrayEditor(Target=ttkBlockAggregator1,
     Source=ttkCinemaProductReader1)
 ttkArrayEditor1.EditorMode = 'Add Arrays from Source'
 ttkArrayEditor1.TargetAttributeType = 'Field Data'
 ttkArrayEditor1.SourceFieldDataArrays = ['_ttk_IterationInfo']
 
 # create a new 'TTK TrackingFromOverlap'
-ttkTrackingFromOverlap1 = TTKTrackingFromOverlap(registrationName='TTKTrackingFromOverlap1', Input=ttkArrayEditor1)
-ttkTrackingFromOverlap1.Labels = 'NONE'
+ttkTrackingFromOverlap1 = TTKTrackingFromOverlap(Input=ttkArrayEditor1)
 ttkTrackingFromOverlap1.Labels = 'RegionId'
 
 # create a new 'TTK EndFor'
-ttkEndFor1 = TTKEndFor(registrationName='TTKEndFor1', Data=ttkTrackingFromOverlap1,
+ttkEndFor1 = TTKEndFor(Data=ttkTrackingFromOverlap1,
     For=ttkForEach1)
 
 # create a new 'Calculator'
-calculator1 = Calculator(registrationName='Calculator1', Input=ttkEndFor1)
+calculator1 = Calculator(Input=ttkEndFor1)
 calculator1.ResultArrayName = 'Size'
 calculator1.Function = 'Size/6000'
 calculator1.ResultArrayType = 'Float'
 
 # create a new 'TTK PlanarGraphLayout'
-ttkPlanarGraphLayout1 = TTKPlanarGraphLayout(registrationName='TTKPlanarGraphLayout1', Input=calculator1)
+ttkPlanarGraphLayout1 = TTKPlanarGraphLayout(Input=calculator1)
 ttkPlanarGraphLayout1.UseSequences = 1
-ttkPlanarGraphLayout1.SequenceArray = ['POINTS', 'NONE']
 ttkPlanarGraphLayout1.SequenceArray = ['POINTS', 'SequenceIndex']
 ttkPlanarGraphLayout1.UseSizes = 1
-ttkPlanarGraphLayout1.SizeArray = ['POINTS', 'NONE']
 ttkPlanarGraphLayout1.SizeArray = ['POINTS', 'Size']
 ttkPlanarGraphLayout1.UseBranches = 1
-ttkPlanarGraphLayout1.BranchArray = ['POINTS', 'NONE']
 ttkPlanarGraphLayout1.BranchArray = ['POINTS', 'BranchId']
 ttkPlanarGraphLayout1.UseLevels = 1
-ttkPlanarGraphLayout1.LevelArray = ['POINTS', 'NONE']
 ttkPlanarGraphLayout1.LevelArray = ['POINTS', 'LevelIndex']
 
 # create a new 'Calculator'
-calculator2 = Calculator(registrationName='Calculator2', Input=ttkPlanarGraphLayout1)
+calculator2 = Calculator(Input=ttkPlanarGraphLayout1)
 calculator2.CoordinateResults = 1
 calculator2.Function = 'iHat*SequenceIndex+jHat*Layout_Y+kHat*LevelIndex'
 
 # create a new 'TTK MeshGraph'
-ttkMeshGraph1 = TTKMeshGraph(registrationName='TTKMeshGraph1', Input=calculator2)
-ttkMeshGraph1.SizeArray = ['POINTS', 'NONE']
+ttkMeshGraph1 = TTKMeshGraph(Input=calculator2)
 ttkMeshGraph1.SizeArray = ['POINTS', 'Size']
 
 # create a new 'Threshold'
-threshold5 = Threshold(registrationName='Threshold5', Input=ttkMeshGraph1)
-threshold5.Scalars = ['POINTS', 'NONE']
+threshold5 = Threshold(Input=ttkMeshGraph1)
 threshold5.Scalars = ['POINTS', 'LevelIndex']
-threshold5.ThresholdRange = [2.0, 2.0]
+threshold5.ThresholdMethod = "Between"
+threshold5.LowerThreshold = 2.0
+threshold5.UpperThreshold = 2.0
 
 # create a new 'Calculator'
-calculator3 = Calculator(registrationName='Calculator3', Input=threshold5)
+calculator3 = Calculator(Input=threshold5)
 calculator3.CoordinateResults = 1
 calculator3.Function = 'coords+kHat*Size/100'
 
 # create a new 'Threshold'
-threshold4 = Threshold(registrationName='Threshold4', Input=ttkMeshGraph1)
-threshold4.Scalars = ['POINTS', 'NONE']
+threshold4 = Threshold(Input=ttkMeshGraph1)
 threshold4.Scalars = ['POINTS', 'LevelIndex']
 
 # create a new 'Threshold'
-threshold6 = Threshold(registrationName='Threshold6', Input=ttkMeshGraph1)
-threshold6.Scalars = ['POINTS', 'NONE']
+threshold6 = Threshold(Input=ttkMeshGraph1)
 threshold6.Scalars = ['POINTS', 'LevelIndex']
-threshold6.ThresholdRange = [1.0, 1.0]
+threshold6.ThresholdMethod = "Between"
+threshold6.LowerThreshold = 1.0
+threshold6.UpperThreshold = 1.0
 
 # ----------------------------------------------------------------
 # setup the visualization in view 'renderView1'
@@ -357,7 +353,7 @@ ttkMeshGraph1Display.OpacityArrayName = [None, '']
 levelIndexLUTColorBar = GetScalarBar(levelIndexLUT, renderView2)
 levelIndexLUTColorBar.AutoOrient = 0
 levelIndexLUTColorBar.Orientation = 'Horizontal'
-levelIndexLUTColorBar.WindowLocation = 'LowerCenter'
+levelIndexLUTColorBar.WindowLocation = 'Lower Center'
 levelIndexLUTColorBar.Title = 'LevelIndex'
 levelIndexLUTColorBar.ComponentTitle = ''
 levelIndexLUTColorBar.TitleFontSize = 24
