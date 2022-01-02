ea803ef4ad889d41e276a806fd70398461e76085
Julien Tierny <julien.tierny@gmail.com>
2022-01-02 09:36:11 +0100

    pv 5.10 uncertainStartingVortex

--- python/uncertainStartingVortex.py
+++ python/uncertainStartingVortex.py
@@ -2,6 +2,18 @@
 
 from paraview.simple import *
 
+# paraview 5.9 VS 5.10 compatibility ===========================================
+def ThresholdBetween(threshold, lower, upper):
+    try:
+        # paraview 5.9
+        threshold.ThresholdRange = [lower, upper]
+    except:
+        # paraview 5.10
+        threshold.ThresholdMethod = "Between"
+        threshold.LowerThreshold = lower
+        threshold.UpperThreshold = upper
+# end of comphatibility ========================================================
+
 # create a new 'XML Image Data Reader'
 uncertainStartingVortexvti = XMLImageDataReader(FileName=['uncertainStartingVortex.vti'])
 
@@ -14,12 +26,12 @@ tTKMandatoryCriticalPoints1.NormalizedThreshold = 0.02
 # create a new 'Threshold' for the Minimas
 threshold6 = Threshold(Input=tTKMandatoryCriticalPoints1)
 threshold6.Scalars = ['POINTS', 'MinimumComponents']
-threshold6.ThresholdRange = [0.0, 2.0]
+ThresholdBetween(threshold6, 0.0, 2.0)
 
 # create a new 'Threshold' for the maximas
 threshold5 = Threshold(Input=OutputPort(tTKMandatoryCriticalPoints1,3))
 threshold5.Scalars = ['POINTS', 'MaximumComponents']
-threshold5.ThresholdRange = [0.0, 8.0]
+ThresholdBetween(threshold5, 0.0, 8.0)
 
 # create a new 'Random Attributes'
 randomAttributes1 = RandomAttributes(Input=uncertainStartingVortexvti)
@@ -39,12 +51,12 @@ tTKPersistenceDiagram1.ScalarField = ['POINTS', 'realization0']
 # create a new 'Threshold'
 threshold2 = Threshold(Input=tTKPersistenceDiagram1)
 threshold2.Scalars = ['CELLS', 'PairIdentifier']
-threshold2.ThresholdRange = [0.0, 185374.0]
+ThresholdBetween(threshold2, 0.0, 185374.0)
 
 # create a new 'Threshold'
 persistenceThreshold1 = Threshold(Input=threshold2)
 persistenceThreshold1.Scalars = ['CELLS', 'Persistence']
-persistenceThreshold1.ThresholdRange = [0.05, 1.75475390406744]
+ThresholdBetween(persistenceThreshold1, 0.05, 1.75475390406744)
 
 # create a new 'TTK TopologicalSimplification'
 tTKTopologicalSimplification1 = TTKTopologicalSimplification(Domain=calculator1,
