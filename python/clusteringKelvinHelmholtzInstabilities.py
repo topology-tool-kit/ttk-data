deb2a431edd227b0a62e71b8e27a63ab04359bb0
Julien J Tierny <julien.tierny@gmail.com>
2022-09-22 17:42:57 +0200

    enable all cores for khi

---
+++ python/clusteringKelvinHelmholtzInstabilities.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+
+from paraview.simple import *
+
+# paraview 5.9 VS 5.10 compatibility ===========================================
+def ThresholdBetween(threshold, lower, upper):
+    try:
+        # paraview 5.9
+        threshold.ThresholdRange = [lower, upper]
+    except:
+        # paraview 5.10
+        threshold.ThresholdMethod = "Between"
+        threshold.LowerThreshold = lower
+        threshold.UpperThreshold = upper
+
+
+# end of comphatibility ========================================================
+
+# create a new 'TTK CinemaReader'
+tTKCinemaReader1 = TTKCinemaReader(DatabasePath='khi.cdb')
+
+# create a new 'TTK CinemaQuery'
+tTKCinemaQuery1 = TTKCinemaQuery(InputTable=tTKCinemaReader1)
+tTKCinemaQuery1.SQLStatement = """
+    SELECT * FROM InputTable0
+        WHERE Resolution='512' 
+            AND (Time='0' OR Time='2') 
+            AND (NOT (Solver='hll'))"""
+
+# create a new 'TTK CinemaProductReader'
+tTKCinemaProductReader1 = TTKCinemaProductReader(Input=tTKCinemaQuery1)
+
+# create a new 'TTK TriangulationManager'
+tTKTriangulationManager1 = TTKTriangulationManager(Input=tTKCinemaProductReader1)
+tTKTriangulationManager1.PeriodicityinAllDimensions = 1
+
+# create a new 'TTK ScalarFieldNormalizer'
+tTKScalarFieldNormalizer1 = TTKScalarFieldNormalizer(Input=tTKTriangulationManager1)
+tTKScalarFieldNormalizer1.ScalarField = ['POINTS', 'Enstrophy']
+
+# create a new 'TTK PersistenceDiagram'
+tTKPersistenceDiagram1 = TTKPersistenceDiagram(Input=tTKScalarFieldNormalizer1)
+tTKPersistenceDiagram1.ScalarField = ['POINTS', 'Enstrophy']
+tTKPersistenceDiagram1.InputOffsetField = ['POINTS', 'Enstrophy']
+
+# create a new 'TTK PersistenceDiagramClustering'
+tTKPersistenceDiagramClustering1 = TTKPersistenceDiagramClustering(Input=tTKPersistenceDiagram1)
+tTKPersistenceDiagramClustering1.Criticalpairsusedfortheclustering = 'saddle-max pairs'
+tTKPersistenceDiagramClustering1.Numberofclusters = 2
+
+# create a new 'TTK PersistenceDiagramDistanceMatrix'
+tTKPersistenceDiagramDistanceMatrix1 = TTKPersistenceDiagramDistanceMatrix(Input=tTKPersistenceDiagramClustering1)
+tTKPersistenceDiagramDistanceMatrix1.Criticalpairsused = 'saddle-max pairs'
+
+# create a new 'TTK LDistanceMatrix'
+tTKLDistanceMatrix1 = TTKLDistanceMatrix(Input=tTKCinemaProductReader1)
+tTKLDistanceMatrix1.ScalarField = ['POINTS', 'Enstrophy']
+
+# create a new 'TTK DimensionReduction'
+tTKDimensionReduction2 = TTKDimensionReduction(Input=tTKLDistanceMatrix1,
+    ModulePath='default')
+tTKDimensionReduction2.Regexp = "Dataset.*"
+tTKDimensionReduction2.SelectFieldswithaRegexp = 1
+tTKDimensionReduction2.InputIsaDistanceMatrix = 1
+
+# create a new 'Table To Points'
+tableToPoints2 = TableToPoints(Input=tTKDimensionReduction2)
+tableToPoints2.XColumn = 'Component_0'
+tableToPoints2.YColumn = 'Component_1'
+tableToPoints2.a2DPoints = 1
+tableToPoints2.KeepAllDataArrays = 1
+
+# create a new 'K Means'
+kMeans1 = KMeans(Input=tableToPoints2,
+    ModelInput=None)
+kMeans1.VariablesofInterest = ['Component_0', 'Component_1']
+kMeans1.k = 2
+
+# create a new 'TTK DimensionReduction'
+tTKDimensionReduction1 = TTKDimensionReduction(Input=tTKPersistenceDiagramDistanceMatrix1,
+    ModulePath='default')
+tTKDimensionReduction1.Regexp = "Diagram.*"
+tTKDimensionReduction1.SelectFieldswithaRegexp = 1
+tTKDimensionReduction1.InputIsaDistanceMatrix = 1
+
+# create a new 'Table To Points'
+tableToPoints1 = TableToPoints(Input=tTKDimensionReduction1)
+tableToPoints1.XColumn = 'Component_0'
+tableToPoints1.YColumn = 'Component_1'
+tableToPoints1.a2DPoints = 1
+tableToPoints1.KeepAllDataArrays = 1
+
+SaveData("W2clusteringAndW2dimensionReduction.csv", tableToPoints1)
+
+SaveData("L2dimensionReductionAndClustering.csv", OutputPort(kMeans1, 1))
