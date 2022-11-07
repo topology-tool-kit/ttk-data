4609b60d5cd179e441fd09cfe47eec0ecb0f8515
MatPont <matthieu.pont@hotmail.fr>
2022-11-07 09:11:15 +0100

    [MT-PGA] add python script and doc

---
+++ python/mergeTreePGA.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+from paraview.simple import *
+
+# create a new 'TTK CinemaReader'
+tTKCinemaReader1 = TTKCinemaReader(DatabasePath='./Earthquake.cdb')
+
+# create a new 'TTK CinemaProductReader'
+tTKCinemaProductReader1 = TTKCinemaProductReader(Input=tTKCinemaReader1)
+tTKCinemaProductReader1.AddFieldDataRecursively = 1
+
+# create a new 'TTK Merge and Contour Tree (FTM)'
+tTKMergeandContourTreeFTM1 = TTKMergeandContourTreeFTM(Input=tTKCinemaProductReader1)
+tTKMergeandContourTreeFTM1.ScalarField = ['POINTS', 'VectorMag']
+tTKMergeandContourTreeFTM1.InputOffsetField = ['POINTS', 'VectorMag']
+tTKMergeandContourTreeFTM1.TreeType = 'Split Tree'
+tTKMergeandContourTreeFTM1.UseAllCores = 0
+
+# create a new 'TTK BlockAggregator'
+tTKBlockAggregator1 = TTKBlockAggregator(Input=[tTKMergeandContourTreeFTM1, OutputPort(tTKMergeandContourTreeFTM1,1), OutputPort(tTKMergeandContourTreeFTM1,2)])
+tTKBlockAggregator1.FlattenInput = 0
+
+# create a new 'TTK MergeTreePrincipalGeodesics'
+tTKMergeTreePrincipalGeodesics1 = TTKMergeTreePrincipalGeodesics(Input=tTKBlockAggregator1,
+    OptionalInput=None)
+tTKMergeTreePrincipalGeodesics1.BarycenterSizeLimitPercent = 17.0
+tTKMergeTreePrincipalGeodesics1.Deterministic = 1
+tTKMergeTreePrincipalGeodesics1.Epsilon1 = 1.5
+tTKMergeTreePrincipalGeodesics1.Epsilon2 = 35.0
+tTKMergeTreePrincipalGeodesics1.Epsilon3 = 34.0
+tTKMergeTreePrincipalGeodesics1.PersistenceThreshold = 1.0
+tTKMergeTreePrincipalGeodesics1.DeleteMultiPersistencePairs = 1
+
+# save the output
+SaveData("MT-PGA_coef.csv", OutputPort(tTKMergeTreePrincipalGeodesics1, 1))
