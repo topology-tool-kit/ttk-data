b9cc01166416a9a218c39e70d756c1c14199f660
Michael Will <mswill@rhrk.uni-kl.de>
2023-11-01 11:31:09 +0100

    Added fixes after merging both merge tree algorithms

--- python/mergeTreeExTreeM.py
+++ python/mergeTreeExTreeM.py
@@ -10,7 +10,7 @@ from paraview.simple import *
 #### disable automatic camera reset on 'Show'
 
 # create a new 'XML Image Data Reader'
-atvti = XMLImageDataReader(registrationName='at.vti', FileName=['/Users/mswill/Documents/phd/ttk-data/at.vti'])
+atvti = XMLImageDataReader(registrationName='at.vti', FileName=['at.vti'])
 atvti.PointArrayStatus = ['density']
 atvti.TimeArray = 'None'
 
@@ -23,13 +23,17 @@ tTKPathCompression1.MorseSmaleComplexHash = 0
 
 # create a new 'TTK MergeTree'
 tTKMergeTree1 = TTKMergeTree(registrationName='TTKMergeTree1', Input=tTKPathCompression1)
-tTKMergeTree1.ScalarArray = ['POINTS', 'density']
-tTKMergeTree1.Type = 'Split Tree'
+tTKMergeTree1.ScalarField = ['POINTS', 'density']
+tTKMergeTree1.InputOffsetField = ['POINTS', 'density']
+tTKMergeTree1.TreeType = 'Split Tree'
+tTKMergeTree1.Backend = 'ExTreeM (IEEE TVCG 2023)'
 
 # create a new 'TTK MergeTree'
 tTKMergeTree2 = TTKMergeTree(registrationName='TTKMergeTree2', Input=tTKPathCompression1)
-tTKMergeTree2.ScalarArray = ['POINTS', 'density']
-tTKMergeTree2.Type = 'Join Tree'
+tTKMergeTree2.ScalarField = ['POINTS', 'density']
+tTKMergeTree2.InputOffsetField = ['POINTS', 'density']
+tTKMergeTree2.TreeType = 'Join Tree'
+tTKMergeTree2.Backend = 'ExTreeM (IEEE TVCG 2023)'
 
 
 SaveData('splitTree.vtu', proxy=OutputPort(tTKMergeTree1, 1))
