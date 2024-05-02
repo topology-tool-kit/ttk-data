d0fec353541f9ccd82d8d1cd509e994d268fa502
Florian Wetzels <wetzels@cs.uni-kl.de>
2024-05-02 14:37:37 +0200

    docs and python scripts for path and branch mapping examples

---
+++ docs/mergeTreeBarycenter_pathMapping.md
@@ -0,0 +1,49 @@
+# Merge Tree Barycenter with Path Mapping 
+
+![Path Mapping Barycenter and Distance Matrix example Image]()
+
+## Pipeline description
+This example first loads a multi block of scalar fields from disk.
+Then, the  the Split Tree is computed on each scalar field using the [MergeTree](https://topology-tool-kit.github.io/doc/html/classttkMergeTree.html) module.
+
+All these trees are passed to [MergeTreeClustering](https://topology-tool-kit.github.io/doc/html/classttkMergeTreeClustering.html) to compute a barycenter in the metric space of merge trees defined by the path mapping distance (thus, the number of clusters has to be set to one and the underlying metric to the path mapping distance). 
+
+In addition, a distance matrix for the input trees is computed with [MergeTreeDistanceMatrix](https://topology-tool-kit.github.io/doc/html/classttkMergeTreeDistanceMatrix.html).
+
+The python script computes the barycenter and saves the resulting planar layout as well as the distance matrix in form of a table. The paraview state file additionally renders the matrix as a heatmap.
+
+## ParaView
+To reproduce the above screenshots, go to your [ttk-data](https://github.com/topology-tool-kit/ttk-data) directory and enter the following command:
+``` bash
+paraview states/mergeTreeBarycenter_pathMapping.pvsm
+```
+
+## Python code
+
+``` python  linenums="1"
+--8<-- "python/mergeTreeBarycenter_pathMapping.py"
+```
+
+To run the above Python script, go to your [ttk-data](https://github.com/topology-tool-kit/ttk-data) directory and enter the following command:
+``` bash
+pvpython python/mergeTreeBarycenter_pathMapping.py
+```
+
+
+## Inputs
+- [outlier.vtm](https://github.com/topology-tool-kit/ttk-data/tree/dev/bdied_outlier/outlier.vtm): a vtk multiblock containing 10 regular grids.
+
+## Outputs
+-  `merge_tree_barycenter.vtm`: the computed barycenter merge tree as a vtk multiblock.
+-  `distance_matrix.csv`: the distance matrix as a csv table.
+
+
+## C++/Python API
+[BlockAggregator](https://topology-tool-kit.github.io/doc/html/classttkBlockAggregator.html)
+
+[MergeTree](https://topology-tool-kit.github.io/doc/html/classttkMergeTree.html)
+
+[MergeTreeClustering](https://topology-tool-kit.github.io/doc/html/classttkMergeTreeClustering.html)
+
+[MergeTreeDistanceMatrix](https://topology-tool-kit.github.io/doc/html/classttkMergeTreeDistanceMatrix.html)
+
---
+++ python/mergeTreeBarycenter_branchMapping.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+# state file generated using paraview version 5.10.1
+
+#### import the simple module from the paraview
+from paraview.simple import *
+
+# ----------------------------------------------------------------
+# setup the data processing pipelines
+# ----------------------------------------------------------------
+
+# create a new 'XML MultiBlock Data Reader'
+outliervtm = XMLMultiBlockDataReader(FileName=['/home/wetzels/ttk/ttk-data_wetzels/bdied_outlier/outlier.vtm'])
+outliervtm.CellArrayStatus = ['vtkGhostType']
+outliervtm.PointArrayStatus = ['test', 'vtkValidPointMask', 'vtkGhostType']
+outliervtm.TimeArray = 'None'
+
+# create a new 'TTK MergeTree'
+tTKMergeandContourTreeFTM1 = TTKMergeTree(Input=outliervtm)
+tTKMergeandContourTreeFTM1.ScalarField = ['POINTS', 'test']
+tTKMergeandContourTreeFTM1.InputOffsetField = ['POINTS', 'test']
+tTKMergeandContourTreeFTM1.TreeType = 'Split Tree'
+tTKMergeandContourTreeFTM1.UseAllCores = 0
+
+# create a new 'TTK BlockAggregator'
+tTKBlockAggregator1 = TTKBlockAggregator(Input=[tTKMergeandContourTreeFTM1, OutputPort(tTKMergeandContourTreeFTM1,1), OutputPort(tTKMergeandContourTreeFTM1,2)])
+tTKBlockAggregator1.FlattenInput = 0
+
+# create a new 'TTK MergeTreeDistanceMatrix'
+tTKMergeTreeDistanceMatrix1 = TTKMergeTreeDistanceMatrix(Input=tTKBlockAggregator1,
+    OptionalInput=None)
+tTKMergeTreeDistanceMatrix1.Backend = 'Branch Mapping Distance (EuroVis 2022)'
+tTKMergeTreeDistanceMatrix1.DistanceSquareRoot = 0
+tTKMergeTreeDistanceMatrix1.Epsilon1 = 0.0
+tTKMergeTreeDistanceMatrix1.Epsilon2 = 100.0
+tTKMergeTreeDistanceMatrix1.Epsilon3 = 100.0
+
+# create a new 'TTK MergeTreeClustering'
+tTKMergeTreeClustering1 = TTKMergeTreeClustering(Input=tTKBlockAggregator1,
+    OptionalInputclustering=None)
+tTKMergeTreeClustering1.Backend = 'Branch Mapping Distance (EuroVis 2022)'
+tTKMergeTreeClustering1.Deterministic = 1
+tTKMergeTreeClustering1.DimensionSpacing = 0.1
+tTKMergeTreeClustering1.DimensionToshift = 'Z'
+tTKMergeTreeClustering1.Epsilon1 = 0.0
+tTKMergeTreeClustering1.Epsilon2 = 100.0
+tTKMergeTreeClustering1.Epsilon3 = 100.0
+tTKMergeTreeClustering1.ImportantPairs = 31.0
+tTKMergeTreeClustering1.ImportantPairsSpacing = 32.0
+tTKMergeTreeClustering1.NonImportantPairsSpacing = 8.0
+
+# save the output
+SaveData("merge_tree_mapping.vtm", proxy=OutputPort(tTKMergeTreeClustering1, 2))
+SaveData("distance_matrix.csv", tTKMergeTreeDistanceMatrix1)
