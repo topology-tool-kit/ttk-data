6d5f47b77f21128c777993c8ec81bdd7bbb29d1e
Julien Tierny <julien.tierny@gmail.com>
2024-05-06 08:41:49 +0200

    simplified and blacked ...branchMapping.py

--- python/mergeTreeBarycenter_branchMapping.py
+++ python/mergeTreeBarycenter_branchMapping.py
@@ -9,38 +9,42 @@ from paraview.simple import *
 # ----------------------------------------------------------------
 
 # create a new 'XML MultiBlock Data Reader'
-outliervtm = XMLMultiBlockDataReader(FileName=['bdied_outlier/outlier.vtm'])
-outliervtm.CellArrayStatus = ['vtkGhostType']
-outliervtm.PointArrayStatus = ['test', 'vtkValidPointMask', 'vtkGhostType']
-outliervtm.TimeArray = 'None'
+outliervtm = XMLMultiBlockDataReader(FileName=["bdied_outlier/outlier.vtm"])
 
 # create a new 'TTK MergeTree'
 tTKMergeandContourTreeFTM1 = TTKMergeTree(Input=outliervtm)
-tTKMergeandContourTreeFTM1.ScalarField = ['POINTS', 'test']
-tTKMergeandContourTreeFTM1.InputOffsetField = ['POINTS', 'test']
-tTKMergeandContourTreeFTM1.TreeType = 'Split Tree'
+tTKMergeandContourTreeFTM1.ScalarField = ["POINTS", "test"]
+tTKMergeandContourTreeFTM1.TreeType = "Split Tree"
 tTKMergeandContourTreeFTM1.UseAllCores = 0
 
 # create a new 'TTK BlockAggregator'
-tTKBlockAggregator1 = TTKBlockAggregator(Input=[tTKMergeandContourTreeFTM1, OutputPort(tTKMergeandContourTreeFTM1,1), OutputPort(tTKMergeandContourTreeFTM1,2)])
+tTKBlockAggregator1 = TTKBlockAggregator(
+    Input=[
+        tTKMergeandContourTreeFTM1,
+        OutputPort(tTKMergeandContourTreeFTM1, 1),
+        OutputPort(tTKMergeandContourTreeFTM1, 2),
+    ]
+)
 tTKBlockAggregator1.FlattenInput = 0
 
 # create a new 'TTK MergeTreeDistanceMatrix'
-tTKMergeTreeDistanceMatrix1 = TTKMergeTreeDistanceMatrix(Input=tTKBlockAggregator1,
-    OptionalInput=None)
-tTKMergeTreeDistanceMatrix1.Backend = 'Branch Mapping Distance (EuroVis 2022)'
+tTKMergeTreeDistanceMatrix1 = TTKMergeTreeDistanceMatrix(
+    Input=tTKBlockAggregator1, OptionalInput=None
+)
+tTKMergeTreeDistanceMatrix1.Backend = "Branch Mapping Distance (EuroVis 2022)"
 tTKMergeTreeDistanceMatrix1.DistanceSquareRoot = 0
 tTKMergeTreeDistanceMatrix1.Epsilon1 = 0.0
 tTKMergeTreeDistanceMatrix1.Epsilon2 = 100.0
 tTKMergeTreeDistanceMatrix1.Epsilon3 = 100.0
 
 # create a new 'TTK MergeTreeClustering'
-tTKMergeTreeClustering1 = TTKMergeTreeClustering(Input=tTKBlockAggregator1,
-    OptionalInputclustering=None)
-tTKMergeTreeClustering1.Backend = 'Branch Mapping Distance (EuroVis 2022)'
+tTKMergeTreeClustering1 = TTKMergeTreeClustering(
+    Input=tTKBlockAggregator1, OptionalInputclustering=None
+)
+tTKMergeTreeClustering1.Backend = "Branch Mapping Distance (EuroVis 2022)"
 tTKMergeTreeClustering1.Deterministic = 1
 tTKMergeTreeClustering1.DimensionSpacing = 0.1
-tTKMergeTreeClustering1.DimensionToshift = 'Z'
+tTKMergeTreeClustering1.DimensionToshift = "Z"
 tTKMergeTreeClustering1.Epsilon1 = 0.0
 tTKMergeTreeClustering1.Epsilon2 = 100.0
 tTKMergeTreeClustering1.Epsilon3 = 100.0
