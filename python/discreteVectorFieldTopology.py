bd7289f024a739a28481904e05a5d45c592708af
finkentA <112194875+finkentA@users.noreply.github.com>
2025-04-23 15:01:23 -0700

    Remove Registration Names

--- python/discreteVectorFieldTopology.py
+++ python/discreteVectorFieldTopology.py
@@ -3,31 +3,31 @@
 from paraview.simple import *
 
 # create a new 'XML Unstructured Grid Reader'
-changesvtu = XMLUnstructuredGridReader(registrationName='changes.vtu', FileName=['changes.vtu'])
+changesvtu = XMLUnstructuredGridReader(FileName=['changes.vtu'])
 changesvtu.PointArrayStatus = ['VectorField']
 changesvtu.TimeArray = 'None'
 
 # create a new 'Random Vectors'
-randomVectors1 = RandomVectors(registrationName='RandomVectors1', Input=changesvtu)
+randomVectors1 = RandomVectors(Input=changesvtu)
 randomVectors1.MaximumSpeed = 0.3
 
 # create a new 'Calculator'
-calculator1 = Calculator(registrationName='Calculator1', Input=randomVectors1)
+calculator1 = Calculator(Input=randomVectors1)
 calculator1.ResultArrayName = 'VectorsWithNoise'
 calculator1.Function = 'VectorField+BrownianVectors'
 
 # create a new 'TTK VectorWeightCurve'
-tTKVectorWeightCurve1 = TTKVectorWeightCurve(registrationName='TTKVectorWeightCurve1', Input=calculator1)
+tTKVectorWeightCurve1 = TTKVectorWeightCurve(Input=calculator1)
 tTKVectorWeightCurve1.InputArray = ['POINTS', 'VectorsWithNoise']
 
 # create a new 'TTK TopologicalSkeleton'
-tTKTopologicalSkeleton1 = TTKTopologicalSkeleton(registrationName='TTKTopologicalSkeleton1', Input=calculator1)
+tTKTopologicalSkeleton1 = TTKTopologicalSkeleton(Input=calculator1)
 tTKTopologicalSkeleton1.VectorField = ['POINTS', 'VectorsWithNoise']
 tTKTopologicalSkeleton1.RunSimplification = 1
 tTKTopologicalSkeleton1.SimplificationThreshold = 27.0
 
 # create a new 'TTK IcospheresFromPoints'
-tTKIcospheresFromPoints1 = TTKIcospheresFromPoints(registrationName='TTKIcospheresFromPoints1', Input=tTKTopologicalSkeleton1)
+tTKIcospheresFromPoints1 = TTKIcospheresFromPoints(Input=tTKTopologicalSkeleton1)
 tTKIcospheresFromPoints1.Radius = 6.5
 
 # save the output
