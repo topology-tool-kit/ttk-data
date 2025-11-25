cb86684d37d1a99ce776137c7ffa433268d38a0d
Julien Tierny <julien.tierny@gmail.com>
2025-11-25 14:53:52 +0100

    [doc] updated gui entries for TopoAE++

--- python/topoAEppTeaser.py
+++ python/topoAEppTeaser.py
@@ -6,7 +6,7 @@ twistedEllipsecsv = CSVReader(FileName=["twistedEllipse.csv"])
 tTKDimensionReduction1 = TTKDimensionReduction(Input=twistedEllipsecsv)
 tTKDimensionReduction1.InputColumns = ["x", "y", "z"]
 tTKDimensionReduction1.Method = "Autoencoder"
-tTKDimensionReduction1.Lossfunction = "Asymmetric Cascade Autoencoder"
+tTKDimensionReduction1.Lossfunction = "Asymmetric Cascade Autoencoder (TopoAE++)"
 tTKDimensionReduction1.Numberofepochs = 1000
 tTKDimensionReduction1.Hiddenlayers = "32 32"
 tTKDimensionReduction1.Enforcedeterminism = 1
@@ -17,7 +17,7 @@ k4csv = CSVReader(FileName=["K4.csv"])
 tTKDimensionReduction2 = TTKDimensionReduction(Input=k4csv)
 tTKDimensionReduction2.InputColumns = ["x", "y", "z"]
 tTKDimensionReduction2.Method = "Autoencoder"
-tTKDimensionReduction2.Lossfunction = "Asymmetric Cascade Autoencoder"
+tTKDimensionReduction2.Lossfunction = "Asymmetric Cascade Autoencoder (TopoAE++)"
 tTKDimensionReduction1.Numberofepochs = 1000
 tTKDimensionReduction2.Hiddenlayers = "128 32"
 tTKDimensionReduction2.Enforcedeterminism = 1
@@ -32,7 +32,7 @@ thresholdTable.MaxValue = 1.0
 tTKDimensionReduction3 = TTKDimensionReduction(Input=thresholdTable)
 tTKDimensionReduction3.InputColumns = sorted([str(x) for x in range(1, 1025)])
 tTKDimensionReduction3.Method = "Autoencoder"
-tTKDimensionReduction3.Lossfunction = "Asymmetric Cascade Autoencoder"
+tTKDimensionReduction3.Lossfunction = "Asymmetric Cascade Autoencoder (TopoAE++)"
 tTKDimensionReduction3.Numberofepochs = 1000
 tTKDimensionReduction3.Hiddenlayers = "128 32"
 tTKDimensionReduction3.Enforcedeterminism = 1
