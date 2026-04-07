170b6a644f59127fe9637e664dd812f3a3e053f7
Julien Tierny <julien.tierny@gmail.com>
2026-04-07 12:19:58 +0200

    [molecularVibration] simplified python script

--- python/molecularVibration.py
+++ python/molecularVibration.py
@@ -1,77 +1,54 @@
 #!/usr/bin/env python
-import paraview
-paraview.compatibility.major = 5
-paraview.compatibility.minor = 13
-
 from paraview.simple import *
 
 # create a new 'TTK CinemaReader'
-tTKCinemaReader1 = TTKCinemaReader(DatabasePath='molecularVibration.cdb')
+tTKCinemaReader1 = TTKCinemaReader(DatabasePath="molecularVibration.cdb")
 
 # create a new 'TTK CinemaQuery'
 tTKCinemaQuery1 = TTKCinemaQuery(InputTable=tTKCinemaReader1)
 tTKCinemaQuery1.SQLStatement = """SELECT * FROM InputTable0
-WHERE ModeId=0 and GeomID in (10, 0, 20)\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t\t"""
+WHERE ModeId=0 and GeomID in (10, 0, 20)"""
 
 # create a new 'TTK CinemaProductReader'
 tTKCinemaProductReader1 = TTKCinemaProductReader(Input=tTKCinemaQuery1)
 
 # create a new 'Calculator'
 calculator1 = Calculator(Input=tTKCinemaProductReader1)
-calculator1.ResultArrayName = 'opposite'
-calculator1.Function = '-rho'
+calculator1.ResultArrayName = "opposite"
+calculator1.Function = "-rho"
 
 # create a new 'TTK ScalarFieldNormalizer'
 tTKScalarFieldNormalizer1 = TTKScalarFieldNormalizer(Input=calculator1)
-tTKScalarFieldNormalizer1.ScalarField = ['POINTS', 'opposite']
+tTKScalarFieldNormalizer1.ScalarField = ["POINTS", "opposite"]
 
 # create a new 'TTK TopologicalSimplificationByPersistence'
-tTKTopologicalSimplificationByPersistence1 = TTKTopologicalSimplificationByPersistence(Input=tTKScalarFieldNormalizer1)
-tTKTopologicalSimplificationByPersistence1.InputArray = ['POINTS', 'opposite']
+tTKTopologicalSimplificationByPersistence1 = TTKTopologicalSimplificationByPersistence(
+    Input=tTKScalarFieldNormalizer1
+)
+tTKTopologicalSimplificationByPersistence1.InputArray = ["POINTS", "opposite"]
 tTKTopologicalSimplificationByPersistence1.PersistenceThreshold = 5e-05
 
-# create a new 'Contour'
-contour1 = Contour(Input=tTKTopologicalSimplificationByPersistence1)
-contour1.ContourBy = ['POINTS', 'opposite']
-contour1.Isosurfaces = [0.9995]
-contour1.PointMergeMethod = 'Uniform Binning'
-
-# create a new 'Extract Block'
-extractBlock8 = ExtractBlock(Input=contour1)
-extractBlock8.Assembly = 'Hierarchy'
-extractBlock8.Selectors = ['/Root/Block0']
-
-# create a new 'Extract Block'
-extractBlock9 = ExtractBlock(Input=contour1)
-extractBlock9.Assembly = 'Hierarchy'
-extractBlock9.Selectors = ['/Root/Block1']
-
-# create a new 'Extract Block'
-extractBlock10 = ExtractBlock(Input=contour1)
-extractBlock10.Assembly = 'Hierarchy'
-extractBlock10.Selectors = ['/Root/Block2']
-
 # create a new 'TTK MorseSmaleComplex'
-tTKMorseSmaleComplex1 = TTKMorseSmaleComplex(Input=tTKTopologicalSimplificationByPersistence1)
-tTKMorseSmaleComplex1.ScalarField = ['POINTS', 'opposite']
-tTKMorseSmaleComplex1.OffsetField = ['POINTS', 'opposite']
-tTKMorseSmaleComplex1.DiscreteGradientBackend = 'Stochastic algorithm (IEEE TVCG 2012)'
+tTKMorseSmaleComplex1 = TTKMorseSmaleComplex(
+    Input=tTKTopologicalSimplificationByPersistence1
+)
+tTKMorseSmaleComplex1.ScalarField = ["POINTS", "opposite"]
+tTKMorseSmaleComplex1.DiscreteGradientBackend = "Stochastic algorithm (IEEE TVCG 2012)"
 tTKMorseSmaleComplex1.ReturnSaddleConnectors = 1
 tTKMorseSmaleComplex1.SaddleConnectorsPersistenceThreshold = 2.5e-05
 
-# find source
-tTKMorseSmaleComplex1_1 = FindSource('TTKMorseSmaleComplex1')
-
 # create a new 'Threshold'
-threshold1 = Threshold(Input=OutputPort(tTKMorseSmaleComplex1_1,1))
-threshold1.Scalars = ['CELLS', 'SeparatrixType']
-threshold1.UpperThreshold = 0.9
+threshold1 = Threshold(Input=OutputPort(tTKMorseSmaleComplex1, 1))
+threshold1.Scalars = ["CELLS", "SeparatrixType"]
+threshold1.LowerThreshold = 0
+threshold1.UpperThreshold = 0
 
 # create a new 'TTK SeparatrixStability'
 tTKSeparatrixStability1 = TTKSeparatrixStability(Input=threshold1)
 
+# Extract the entry #1, corresponding to the equilibrium state
 extractBlock11 = ExtractBlock(Input=tTKSeparatrixStability1)
-extractBlock11.Assembly = 'Hierarchy'
-extractBlock11.Selectors = ['/Root/Block1']
+extractBlock11.Assembly = "Hierarchy"
+extractBlock11.Selectors = ["/Root/Block1"]
 
 SaveData("bondOccurrenceRate.vtm", extractBlock11)
